85cefd607c6ef0f62812df44c4c9b0f8ff63bfff
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2025-04-09 15:26:04 -0500

    Update round_1.py
    Changed the code a little so now, instead of relying fully on hardcoded values, we now mainly rely on the average sell order price of the previous sell orders for our buy and sell thresholds.

--- round_1.py
+++ round_1.py
@@ -2,7 +2,33 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 
-def getLowestSellOrder(sell_orders):
+def string_to_dictionary(s):
+    s = s.strip("{}")
+    s = s.split("]")
+
+    newList = []
+    for entry in s:
+        if entry != "":
+            newList.append((entry + "]").strip(", "))
+
+    d = {}
+    for item in newList:
+        key_value_pair = item.split(":")
+        key = key_value_pair[0].strip(" '")
+        
+        values = key_value_pair[1].strip(" []").split(",")
+        
+        for index, value in enumerate(values):
+            values[index] = int(value.strip())
+        
+        d[key] = values
+    
+    return d
+
+def get_average(prices):
+    return sum(prices) / len(prices)
+
+def get_lowest_sell_order(sell_orders):
     lowest_price = 0
     associated_amount = 0
 
@@ -18,7 +44,7 @@ def getLowestSellOrder(sell_orders):
     
     return (lowest_price, associated_amount)
 
-def getHighestBuyOrder(buy_orders):
+def get_highest_buy_order(buy_orders):
     highest_price = 0
     associated_amount = 0
 
@@ -71,6 +97,10 @@ class Trader:
 		# Orders to be placed on exchange matching engine
         result = {}
 
+        sell_order_history = {}
+        if state.traderData != "":
+            sell_order_history = string_to_dictionary(state.traderData)
+
         # state.order_depths:
         # keys = products, values = OrderDepth instances
 
@@ -90,17 +120,35 @@ class Trader:
             # Make a list of orders
             orders: List[Order] = []
 
+            """
             # Set to "RAINFOREST_RESIN" price by default
-            acceptable_buy_price = 9999  # Participant should calculate this value
-            acceptable_sell_price = 10001  # Participant should calculate this value
+            acceptable_buy_price = 9998.5  # Participant should calculate this value
+            acceptable_sell_price = 10002  # Participant should calculate this value
 
             if product == "SQUID_INK":
-                acceptable_buy_price = 1950
+                acceptable_buy_price = 1949.5
                 acceptable_sell_price = 1970
             
             elif product == "KELP":
-                acceptable_buy_price = 2030
+                acceptable_buy_price = 2029.5
                 acceptable_sell_price = 2032
+            """
+
+            # Set to "RAINFOREST_RESIN" price by default
+            acceptable_buy_price = 9999  # Participant should calculate this value
+            acceptable_sell_price = 10002  # Participant should calculate this value
+
+            if product == "SQUID_INK":
+                acceptable_buy_price = 1949.5
+                acceptable_sell_price = 1970
+            
+            elif product == "KELP":
+                acceptable_buy_price = 2029.5
+                acceptable_sell_price = 2032
+            
+            if sell_order_history.get(product) is not None:
+                acceptable_buy_price = get_average(sell_order_history[product]) - 2
+                acceptable_sell_price = get_average(sell_order_history[product]) + 3
 
             print(f"Acceptable buy price: {acceptable_buy_price}")
             print(f"Acceptable sell price: {acceptable_sell_price}")
@@ -113,9 +161,14 @@ class Trader:
                 # Get the price and quantity of the first sell?
                 # best_ask = price
                 # best_ask_amount = quantity
-                best_ask, best_ask_amount = getLowestSellOrder(list(order_depth.sell_orders.items()))
+                best_ask, best_ask_amount = get_lowest_sell_order(list(order_depth.sell_orders.items()))
                 print(f"Sell orders: {list(order_depth.sell_orders.items())}")
 
+                if sell_order_history.get(product) is None:
+                    sell_order_history[product] = [best_ask]
+                else:
+                    sell_order_history[product].append(best_ask)
+                
                 # If the bot is selling for less than we expect (wahoo)
                 if int(best_ask) < acceptable_buy_price:
                     # Buy some of that I guess
@@ -127,7 +180,7 @@ class Trader:
                 # Get the price and quantity of the first buy?
                 # best_bid = price
                 # best_bid_amount = quantity
-                best_bid, best_bid_amount = getHighestBuyOrder(list(order_depth.buy_orders.items()))
+                best_bid, best_bid_amount = get_highest_buy_order(list(order_depth.buy_orders.items()))
                 print(f"Buy orders: {list(order_depth.buy_orders.items())}")
 
                 # If the bot is buying for more than we expect (wahoo)
@@ -142,8 +195,8 @@ class Trader:
 
         # String value holding Trader state data required. 
         # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE"
-        
+        traderData = str(sell_order_history)
+
         # Sample conversion request. Check more details below. 
         conversions = 1
         return result, conversions, traderData
