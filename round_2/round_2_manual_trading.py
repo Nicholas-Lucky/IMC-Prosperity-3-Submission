c1a96b249a1248b059d1a23ba94f04c70d9ec07a
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2025-04-11 19:52:00 -0500

    Update round_2_manual_trading.py
    Changed some variable names and a print statement to indicate that the percentages we are calculating and sorting are the maximum percentages for the crates to be profitable

--- round_2/round_2_manual_trading.py
+++ round_2/round_2_manual_trading.py
@@ -3,16 +3,16 @@ class Crate:
         self.initial_multiplier = initial_multiplier
         self.inhabitatnts = inhabitatnts
     
-    def get_min_percentage(self, target_multiplier):
+    def get_max_percentage(self, target_multiplier):
         return (self.initial_multiplier / target_multiplier) - self.inhabitatnts
     
     def __repr__(self):
         return f"Crate({self.initial_multiplier}, {self.inhabitatnts})"
 
-def sort_by_percent(crates, percent_to_be_profitable):
+def sort_by_percent(crates, max_percent_to_be_profitable):
     for i in range(0, len(crates) - 1):
         for j in range(0, len(crates) - 1 - i):
-            if crates[j].get_min_percentage(percent_to_be_profitable) > crates[j + 1].get_min_percentage(percent_to_be_profitable):
+            if crates[j].get_max_percentage(max_percent_to_be_profitable) > crates[j + 1].get_max_percentage(max_percent_to_be_profitable):
                 temp = crates[j]
                 crates[j] = crates[j + 1]
                 crates[j + 1] = temp
@@ -31,14 +31,14 @@ test(a)
 print(a)
 """
 
-percent_to_be_profitable = 5
+max_percent_to_be_profitable = 5
 crates = [Crate(10, 1), Crate(80, 6), Crate(37, 3),
           Crate(90, 10), Crate(31, 2), Crate(50, 4),
           Crate(20, 2), Crate(73, 4), Crate(89, 8)]
 
-sort_by_percent(crates, percent_to_be_profitable)
+sort_by_percent(crates, max_percent_to_be_profitable)
 
-print("Min % of people needed for crate to be profitable (lowest to highest):")
+print("MAX % of people needed for crate to be profitable (lowest to highest):")
 print("Note: Formatting is Crate(multiplier, inhabitants)")
 for crate in crates:
-    print(f"{crate}: {crate.get_min_percentage(percent_to_be_profitable)}%")
+    print(f"{crate}: {crate.get_max_percentage(max_percent_to_be_profitable)}%")
